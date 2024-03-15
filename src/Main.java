8a84049cdd1d875640c9b853d53e9560f2f21cfc
Waylon! <weese.619547@swl.k12.oh.us>
2024-03-15 00:07:15 -0400

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double balance = 5000;
+        double interestRate = 0.17; // Monthly interest rate
+        double balanceAfterOneMonth = balance * (1 + interestRate);
+        double balanceAfterTwoMonths = balanceAfterOneMonth * (1 + interestRate);
+
+        System.out.println("Credit Card Balance: $" + balance);
+        System.out.println("Credit Card Balance after One Month: $" + balanceAfterOneMonth);
+        System.out.println("Credit Card Balance after Two Months: $" + balanceAfterTwoMonths);
     }
 }
